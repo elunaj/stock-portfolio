b28b0afd14b8719a4b818ba80c2804edf2573656
elunaj <elias.luna@mail.citytech.cuny.edu>
2020-02-29 14:40:51 -0500

    troubleshooting 2

--- server.js
+++ server.js
@@ -47,9 +47,11 @@ const db = knex({
 });
 
 // API endpoints
-app.get('/', (req, res) => {
-	res.send('Root!');
-})
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.post('/signin', (req, res) => {
 	signin.handleSignin(req, res, db, bcrypt);
@@ -77,12 +79,6 @@ app.get('/portfolio/:id', (req, res) => {
 
 // Serve static files from the React app
 app.use(express.static(__dirname + '/client/build'));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
 
 // Port connection
 app.listen(PORT, () => {
