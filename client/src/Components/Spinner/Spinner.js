72e04f7d027ca17dd77d818318131f981f8a83d1
elunaj <elias.luna@mail.citytech.cuny.edu>
2020-02-29 04:32:10 -0500

    spinner for portfolio loading

---
+++ client/src/Components/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import './Spinner.css';
+
+export default function CircularUnderLoad() {
+  
+  return (<CircularProgress className="spinner"
+
+  		disableShrink />
+  	);
+}
